bf9bd9f5da9bd4efeb450af061c256021a08bbbf
Christian Schulte <schulte@connectiv.de>
2017-03-09 12:11:39 +0100

    update scripts

--- Scripts/replaceChartWithScheduler.js
+++
@@ -1,6 +0,0 @@
-function replaceChartWithScheduler() {
-	var iframe = '<iframe name="WebResource_sched" tabindex="0" class="ms-crm-Custom" id="WebResource_sched" src="/%7B635804828000003149%7D/WebResources/con_/html/con_kendo_schedule_workingtime.html" frameborder="0" scrolling="auto" preload="0" url="/%7B635804828000003149%7D/WebResources/con_/html/con_kendo_schedule_workingtime.html" delayinitialize="0"></iframe>';
-	$( "iframe" ).contents().find( "#crmGrid_pane" ).prepend( iframe );
-}
-
-replaceChartWithScheduler();
